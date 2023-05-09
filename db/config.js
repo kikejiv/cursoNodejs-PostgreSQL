7a0ff3f9629d4fb401e4a50d6cd21b1b94a048f5
Jose Valencia <tecnicojiv@gmail.com>
2023-05-09 10:26:21 -0500

    paso-10 configuracion inicial para correr migraciones

---
+++ db/config.js
@@ -0,0 +1,17 @@
+//en este archivo se ponen los archivos de conexion a la base de datos
+const { config } = require('./../config/config'); //importo el modulo de config
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`; //se concatenn las variables de conexion
+
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  },
+}
